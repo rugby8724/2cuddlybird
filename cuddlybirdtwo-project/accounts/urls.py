4a3d8719d7a91eb74ebcb507b5a3356286c6bcc3
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2020-02-10 19:35:41 -0600

    Add Forgot Password

--- cuddlybirdtwo-project/accounts/urls.py
+++ cuddlybirdtwo-project/accounts/urls.py
@@ -8,5 +8,18 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.Signup.as_view(), name='signup'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'),name='password_reset_complete'),
+
 
 ]
